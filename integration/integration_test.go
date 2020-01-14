82b1d7fbd7c6c869b9c22d99ae1bb47758dec4ee
Maël Valais <mael.valais@gmail.com>
2020-01-14 10:09:19 +0100

    try subtests + testify instead of ginkgo + gomega
    Why use plan subtests instead of Ginkgo and not benefit from all the
    Ginkgo goodness ('f' focus mode, watcher...)? I feel sometimes it's just
    simpler to go with nested t.Run (instead of Describe + When +It) and use
    a simple assertion lib instead of Gomega:
    
        Expect(err).NotTo(HaveOccured())
    
        versus
    
        require.NoError(t, err)
    
    Also, Ginkgo has a lot of 'global' behavior and it can be challenging to
    mix non-ginkgo tests with ginkgo ones. Typically, one would write
    
        var _ = Describe("...", func() {})

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,150 +1,186 @@
 package integration_test
 
 import (
+	"github.com/stretchr/testify/assert"
 	"io"
 	"os/exec"
+	"testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("Integration", func() {
-	var (
-		inBuf  *gbytes.Buffer
-		cliCmd *exec.Cmd
-	)
+var cliBin string
 
-	BeforeEach(func() {
+func BeforeSuite() {
+	var err error
+	cliBin, err = gexec.Build("github.com/mo-work/go-technical-test-for-claudia/cmd", "-mod=vendor")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AfterSuite() {
+	gexec.Terminate()
+	gexec.CleanupBuildArtifacts()
+}
+
+func TestIntegration(t *testing.T) {
+	BeforeSuite()
+	defer AfterSuite()
+
+	BeforeEach := func() (inBuf *gbytes.Buffer, cliCmd *exec.Cmd) {
 		cliCmd = exec.Command(cliBin)
 		inBuf = gbytes.NewBuffer()
 		cliCmd.Stdin = inBuf
-	})
+		return inBuf, cliCmd
+	}
 
-	Describe("'I': setting image size", func() {
-		Context("when executing the program", func() {
-			It("the user can enter a image size", func() {
+	t.Run("'I': setting image size", func(t *testing.T) {
+		t.Run("when executing the program", func(t *testing.T) {
+			t.Run("the user can enter a image size", func(t *testing.T) {
+				inBuf, cliCmd := BeforeEach()
+
 				_, err := io.WriteString(inBuf, "I 5 5")
-				Expect(err).NotTo(HaveOccurred())
+				assert.NoError(t, err)
 
-				session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(session.Err.Contents).Should(HaveLen(0))
+				bytes, err := cliCmd.CombinedOutput()
+				assert.NoError(t, err)
+				assert.Len(t, bytes, 0)
 			})
 		})
 
-		Context("if the user specifies something other than a image size", func() {
+		t.Run("if the user specifies something other than a image size", func(t *testing.T) {
 			// TODO: this can probably be changed to repeat the request
-			It("the program will complain and exit", func() {
+			t.Run("the program will complain and exit", func(t *testing.T) {
+				inBuf, cliCmd := BeforeEach()
+
 				_, err := io.WriteString(inBuf, "X 5 5")
-				Expect(err).NotTo(HaveOccurred())
+				assert.NoError(t, err)
 
-				session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(session.Out).Should(gbytes.Say("invalid image value: unrecognised command 'X', use 'I' for Image initialisation"))
+				bytes, err := cliCmd.CombinedOutput()
+				assert.NoError(t, err)
+				assert.Equal(t, "invalid image value: unrecognised command 'X', use 'I' for Image initialisation\n", string(bytes))
 			})
 		})
 	})
 
-	Describe("'L': colouring an individual pixel", func() {
-		It("sets the pixel at the given coordinates to a given colour", func() {
+	t.Run("'L': colouring an individual pixel", func(t *testing.T) {
+		t.Run("sets the pixel at the given coordinates to a given colour", func(t *testing.T) {
+			inBuf, cliCmd := BeforeEach()
+
 			_, err := io.WriteString(inBuf, "I 5 5\nL 1 3 A\nS")
-			Expect(err).NotTo(HaveOccurred())
+			assert.NoError(t, err)
 
-			session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(session.Out).Should(gbytes.Say("OOOOO\nOOOOO\nAOOOO\nOOOOO\nOOOOO\n"))
+			bytes, err := cliCmd.CombinedOutput()
+			assert.NoError(t, err)
+			assert.Equal(t, "OOOOO\nOOOOO\nAOOOO\nOOOOO\nOOOOO\n\n", string(bytes))
 		})
 
-		Context("if the action cannot be processed", func() {
-			It("prints an error", func() {
+		t.Run("if the action cannot be processed", func(t *testing.T) {
+			t.Run("prints an error", func(t *testing.T) {
+				inBuf, cliCmd := BeforeEach()
+
 				_, err := io.WriteString(inBuf, "I 5 5\nL 1 6 A")
-				Expect(err).NotTo(HaveOccurred())
+				assert.NoError(t, err)
 
-				session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(session.Out).Should(gbytes.Say("given coordinate is beyond image grid"))
+				bytes, err := cliCmd.CombinedOutput()
+				assert.NoError(t, err)
+				assert.Equal(t, "given coordinate is beyond image grid\n", string(bytes))
 			})
 		})
 	})
 
-	Describe("'V': colouring a vertical line", func() {
-		It("sets the pixels between the specified coordinates", func() {
+	t.Run("'V': colouring a vertical line", func(t *testing.T) {
+		t.Run("sets the pixels between the specified coordinates", func(t *testing.T) {
+			inBuf, cliCmd := BeforeEach()
+
 			_, err := io.WriteString(inBuf, "I 5 5\nV 2 3 5 W\nS")
-			Expect(err).NotTo(HaveOccurred())
+			assert.NoError(t, err)
 
-			session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(session.Out).Should(gbytes.Say("OOOOO\nOOOOO\nOWOOO\nOWOOO\nOWOOO\n"))
+			bytes, err := cliCmd.CombinedOutput()
+			assert.NoError(t, err)
+			assert.Equal(t, "OOOOO\nOOOOO\nOWOOO\nOWOOO\nOWOOO\n\n", string(bytes))
 		})
 
-		Context("if the action cannot be processed", func() {
-			It("prints an error", func() {
+		t.Run("if the action cannot be processed", func(t *testing.T) {
+			t.Run("prints an error", func(t *testing.T) {
+				inBuf, cliCmd := BeforeEach()
+
 				_, err := io.WriteString(inBuf, "I 5 5\nV 7 2 5 W")
-				Expect(err).NotTo(HaveOccurred())
+				assert.NoError(t, err)
 
-				session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(session.Out).Should(gbytes.Say("given coordinate is beyond image grid"))
+				bytes, err := cliCmd.CombinedOutput()
+				assert.NoError(t, err)
+				assert.Equal(t, "given coordinate is beyond image grid\n", string(bytes))
 			})
 		})
 	})
 
-	Describe("'H': colouring a horizontal line", func() {
-		It("sets the pixels between the specified coordinates", func() {
+	t.Run("'H': colouring a horizontal line", func(t *testing.T) {
+		t.Run("sets the pixels between the specified coordinates", func(t *testing.T) {
+			inBuf, cliCmd := BeforeEach()
+
 			_, err := io.WriteString(inBuf, "I 5 5\nH 3 5 2 Z\nS")
-			Expect(err).NotTo(HaveOccurred())
+			assert.NoError(t, err)
 
-			session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(session.Out).Should(gbytes.Say("OOOOO\nOOZZZ\nOOOOO\nOOOOO\nOOOOO\n"))
+			bytes, err := cliCmd.CombinedOutput()
+			assert.NoError(t, err)
+			assert.Equal(t, "OOOOO\nOOZZZ\nOOOOO\nOOOOO\nOOOOO\n\n", string(bytes))
 		})
 
-		Context("if the action cannot be processed", func() {
-			It("prints an error", func() {
+		t.Run("if the action cannot be processed", func(t *testing.T) {
+			t.Run("prints an error", func(t *testing.T) {
+				inBuf, cliCmd := BeforeEach()
+
 				_, err := io.WriteString(inBuf, "I 5 5\nH 3 5 7 Z")
-				Expect(err).NotTo(HaveOccurred())
+				assert.NoError(t, err)
 
-				session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(session.Out).Should(gbytes.Say("given coordinate is beyond image grid"))
+				bytes, err := cliCmd.CombinedOutput()
+				assert.NoError(t, err)
+				assert.Equal(t, "given coordinate is beyond image grid\n", string(bytes))
 			})
 		})
 	})
 
-	Describe("'S': showing the image", func() {
-		Context("whenever the user inputs the 'S' command", func() {
-			It("the image is printed in its current state", func() {
+	t.Run("'S': showing the image", func(t *testing.T) {
+		t.Run("whenever the user inputs the 'S' command", func(t *testing.T) {
+			t.Run("the image is printed in its current state", func(t *testing.T) {
+				inBuf, cliCmd := BeforeEach()
+
 				_, err := io.WriteString(inBuf, "I 5 5\nS")
-				Expect(err).NotTo(HaveOccurred())
+				assert.NoError(t, err)
 
-				session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(session.Out).Should(gbytes.Say("OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\n"))
+				bytes, err := cliCmd.CombinedOutput()
+				assert.NoError(t, err)
+				assert.Equal(t, "OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\n\n", string(bytes))
 			})
 		})
 	})
 
-	Describe("'C': clearing the image", func() {
-		It("the image pixels are cleared", func() {
+	t.Run("'C': clearing the image", func(t *testing.T) {
+		t.Run("the image pixels are cleared", func(t *testing.T) {
+			inBuf, cliCmd := BeforeEach()
+
 			_, err := io.WriteString(inBuf, "I 2 2\nL 1 1 A\nS\nC\nS")
-			Expect(err).NotTo(HaveOccurred())
+			assert.NoError(t, err)
 
-			session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(session.Out).Should(gbytes.Say("AO\nOO\n\nOO\nOO\n"))
+			bytes, err := cliCmd.CombinedOutput()
+			assert.NoError(t, err)
+			assert.Equal(t, "AO\nOO\n\nOO\nOO\n\n", string(bytes))
 		})
 	})
 
-	Context("any other attempted action", func() {
-		It("complains", func() {
+	t.Run("any other attempted action", func(t *testing.T) {
+		t.Run("complains", func(t *testing.T) {
+			inBuf, cliCmd := BeforeEach()
+
 			_, err := io.WriteString(inBuf, "I 2 2\nP 1 1 A\n")
-			Expect(err).NotTo(HaveOccurred())
+			assert.NoError(t, err)
 
-			session, err := gexec.Start(cliCmd, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(session.Out).Should(gbytes.Say("invalid action"))
+			bytes, err := cliCmd.CombinedOutput()
+			assert.NoError(t, err)
+			assert.Equal(t, "invalid action\n", string(bytes))
 		})
 	})
-})
+}
